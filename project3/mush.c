88fc7f0b6b19183ada65b4ffda36d9e8551a9411
achi <chidesta@oregonstate.edu>
2023-01-28 18:28:37 -0800

    Saving char array of strtok'd fget's... lol.

--- project3/mush.c
+++ project3/mush.c
@@ -12,29 +12,33 @@ int main(/* int argc,char *argv[] */ ){
     printf("$");
 
     char input[BUFFER];
+    char *args_array[ALLOWED_WORDS]; 
     fgets(input, sizeof input, stdin);
-    printf("fgets got: %s", input);     // sanity check
+    // printf("fgets got: %s", input);     // sanity check
 
-
-    int i = 0;                   // Now take this and make it add all these to a char* array
+    int i = 0;                  
     char *token = strtok(input, " ");
-    printf("%d: [%p] ---- %s\n", i, token, token);      // sanity check
+    // printf("%d: [%p] ---- %s\n", i, token, token);     // sanity check 
 
-    while(token != NULL && i < ALLOWED_WORDS){
+    while(token != NULL && i < ALLOWED_WORDS){          // adding tokens to a char* array (p1.3)
+        args_array[i] = token;                        
         ++i;
         token = strtok(NULL, " ");
-        printf("%d: [%p] ---- %s\n", i, token, token);  // sanity check
+        // printf("%d: [%p] ---- %s\n", i, token, token);  // sanity check
+    }   
 
+    args_array[i] = NULL; 
+    
+
+    for(i = 0; i < ALLOWED_WORDS; ++i){       //sanity check
+        printf("%s ", args_array[i]);
     }
+
+
 
 }
 
     
-
-
-
-    // You should print a prompt, read a line (fgets()), and then parse it down to individual words (strtok()).
-    // I suggest you stop here and test everything to make sure it's working as expected.
     // Put those words in an array of char*s. Be sure to terminate the array with a NULL so you can pass it easily to execvp().
     // I suggest you stop here as well, and make sure the array is built as you expected. It's easier to debug small bits at a time than the whole thing at once.
     // Call execvp() with your array. Pass the 0th element as the first argument ot execvp().
