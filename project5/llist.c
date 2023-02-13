1b50b7f5367953dafd1871155577b3b5709ca208
achi <chidesta@oregonstate.edu>
2023-02-12 22:33:18 -0800

    had to fix some issues from not using the header file

--- project5/llist.c
+++ project5/llist.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "llist.h"
 
-typedef struct {
-    int value;
-    struct Node* next;
-}Node;
 
-Node* node_alloc(int value){
-    Node* new_node = (Node*)malloc(sizeof(Node));
+struct node *node_alloc(int value){
+    struct node *new_node = malloc(sizeof(struct node));
     new_node->value = value;
     new_node->next = NULL;
     return new_node;
 }
 
-void node_free(Node* node){
-    free(node);
+void llist_insert_head(struct node **head, struct node *n){
+    if(*head == NULL){
+        printf("Succesfully replaced NULL head");
+       *head = n;
+    }
 
+    return;
 }
 
-int main(int argc, char *argv[]){
-    Node *head = NULL;
-    Node *tail = NULL;
 
-    int n;
+void node_free(struct node *n){
+    free(n);
+}
+
+
+int main(int argc, char *argv[]){
+    struct node *head = NULL;
+
 
     for(int i = 1; i < argc; i++){
 
         if(strcmp(argv[i], "ih") == 0){ 
-            Node* node_pointer = node_alloc(atoi(argv[i+1]));
-            head = node_pointer;
-            printf("%s stored as node struct %p", argv[i], node_pointer);
+            struct node *n = node_alloc(atoi(argv[i+1]));
+            llist_insert_head(&head, n);
+
         }
 
         if(strcmp(argv[i], "p") == 0){ 
             printf("\n%d is the head", head->value);
         }
-
 
         //low priority: add check for isdigit (note:atoi will convert char to 0)
         int input_num = atoi(argv[i]);
