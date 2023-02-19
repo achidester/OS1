92690ec495c436c744cbd942fde9cbab5f632413
achi <chidesta@oregonstate.edu>
2023-02-19 15:23:24 -0800

    initial commit. code that was pretty much handed to us in the assignment doc

---
+++ project6/myalloc.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define ALIGNMENT 16   // Must be power of 2
+#define GET_PAD(x) ((ALIGNMENT - 1) - (((x) - 1) & (ALIGNMENT - 1)))
+
+#define PADDED_SIZE(x) ((x) + GET_PAD(x))
+
+
+struct block *head = NULL;  // Head of the list, empty
+
+struct mem *mem(int size){
+
+}
+
+
+
+void main(int argc, char *argv){}
+
+
+
+// given portion of my_malloc
+void my_malloc(){
+    if (head == NULL) {
+        head = mmap(NULL, 1024, PROT_READ|PROT_WRITE,
+                    MAP_ANON|MAP_PRIVATE, -1, 0);
+        head->next = NULL;
+        head->size = 1024 - PADDED_SIZE(sizeof(struct block));
+        head->in_use = 0;
+    }
+}
+
+
+// given print function
+void print_data(void)
+{
+    struct block *b = head;
+
+    if (b == NULL) {
+        printf("[empty]\n");
+        return;
+    }
+
+    while (b != NULL) {
+        // Uncomment the following line if you want to see the pointer values
+        //printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        printf("[%d,%s]", b->size, b->in_use? "used": "free");
+        if (b->next != NULL) {
+            printf(" -> ");
+        }
+
+        b = b->next;
+    }
+
+    printf("\n");
+}
